6c3aab1e7b627a78df75c14f8ef48a8d423257fe
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-09-17 15:53:54 +0200

    Stubs for pages

---
+++ navod.php
@@ -0,0 +1,11 @@
+<?php
+$actual = "navod";
+include('header.php');
+?>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nibh risus, viverra vel rhoncus ut, posuere sed arcu. 
+Etiam quis mattis mi. Ut sodales dui ac aliquet egestas. 
+</p>
+<?php
+include('footer.php');
+?>
---
+++ vysledky.php
@@ -0,0 +1,8 @@
+<?php
+$actual = "vysledky";
+include('header.php');
+?>
+
+<?php
+include('footer.php');
+?>
