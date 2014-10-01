2e1f02a6ea84ead2cad48c2c2ee8224de199effc
kouzelnik <vojkuvka@webalevne.cz>
2014-10-01 07:29:38 +0200

    KOUZELNIK

--- info.php
+++ info.php
@@ -2,7 +2,7 @@
 $actual = "info";
 include('header.php');
 ?>
-Autoři: František Zatloukal, Radek Vojkůvka, Michael Kužela, Vojtěch Górecki
+Autoři: František Zatloukal, Kouzelník, Michael Kužela, Vojtěch Górecki
 <?php
 include('footer.php');
 ?>
