fc2af783397debaeecce9915691d2debf52754e1
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-10-05 15:37:27 +0200

    Reindent

--- index.php
+++ index.php
@@ -6,39 +6,39 @@ include('functions.php');
 // Work around missing functions in old php
 if (phpversion() < 5.5)
 {
-  require_once ('passwordLib.php');
+	require_once ('passwordLib.php');
 }
 
 if (isset($_POST["username_login"]))
 {
   
-  login($_POST["username_login"], $_POST["password_login"]);
+	login($_POST["username_login"], $_POST["password_login"]);
 }
 
 if (isset($_POST["username_logout"]))
 {
-  logout();
+	logout();
 }
 
 if (isset($_GET["stranka"]))
 {
-  $actual = $_GET["stranka"];
+	$actual = $_GET["stranka"];
 }
 else
 {
-  $actual = "index";
+	$actual = "index";
 }
 
 include('header.php');
 
 if (isset($_GET["stranka"]))
 {
-  $inc = $_GET["stranka"] . ".php";
-  include($inc);
+	$inc = $_GET["stranka"] . ".php";
+	include($inc);
 }
 else
 {
-  include("uvod.php");
+	include("uvod.php");
 }
 
 include('footer.php');
