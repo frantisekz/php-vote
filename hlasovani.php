8e07194a224f4bb3cd3825cadd53c082aad9617e
chnupal <chnupal@gmail.com>
2014-10-06 20:24:59 +0200

    Poll update + some fixes 2
    Tak snad jsem tě pochopil.

--- hlasovani.php
+++ hlasovani.php
@@ -2,9 +2,10 @@
 <meta name="keywords" content="" lang="cs" />
 <meta name="author" content="" />
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<style type="text/css">
-	
-</style>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/jquery.textfill.min.js"></script>
+<script type="text/javascript" src="scripts.js"></script>
+
 <header>
 
 	<h1><a href="index.php">Hlasovací<strong>systém</strong></a></h1>
@@ -14,40 +15,36 @@
 	<div class="clear"></div>
 	</header>
 
-
 <body>
-
-
 <div class="body">
 <h2>Zeměpis - 1</h2>
 <fieldset id="Poll">
 	<legend>Kolik zemí je v EU?</legend>	
 	<form action="<?php echo $editFormAction; ?>" id="form1" name="form1" method="POST">
-			
-<div value="1" id="Poll_1" >
-			1
+
+<div value="1" id="Poll_1">
+<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in ipsum sed massa varius volutpat. Nam gravida, velit at iaculis vestibulum, diam lectus scelerisque mauris.</span>
 </div>
-<div class="mezera"></div>
 		
 <div value="2" id="Poll_2" >
-			2
+	<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim.</span>
 </div>
 <div class="mezera"></div>
 			
 <div value="3" id="Poll_3" >
-			3
+	<span>		3</span>
 </div>
-		
-<div class="mezera"></div>
 
 <div value="4" id="Poll_4" >
-			4
-</div>
-	
+	<span>		4</span>
+</div>		
+		
 	</form>
 	</fieldset>
 	</div>
+<div id="diskuze">
 Diskuze k Zeměpis - 1
+		</div>
 <?php
 include('footer.php');
 ?>
