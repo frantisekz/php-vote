5e404fbb1c8f2879a36ffe62af69ecfc315228aa
chnupal <chnupal@gmail.com>
2014-10-09 20:11:54 +0200

    Some fixes
    Male upravy a favicon

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <body>
-	<title>php-vote</title>
+	<title>Hlasovací systém</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="" lang="cs" />
 <meta name="author" content="" />
   <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="icon" type="img/ico" href="img/favicon.ico">
 <style type="text/css">
   </style>	
 	<header>
@@ -39,18 +40,19 @@ else
 		?>
 	</fieldset>
 <ul>
-   <li <?php if ($actual == "index"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy">
+   <li <?php if ($actual == "index"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy" onclick="location.href = 'index.php';">
 	   	<img src="img/index.png" style="height:17px;width:17px" href="index.php">
 	<a  class="odkaz1" href="index.php">Domů</a>
 	</li>
-   <li <?php if ($actual == "navod"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy">
+   <li <?php if ($actual == "navod"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy" onclick="location.href = 'navod.php';">
 		   	<img src="img/navod.png" style="height:17px;width:17px">
 	<a class="odkaz1" href="index.php?stranka=navod">Návod</a>
 	</li>   
-	<li <?php if ($actual == "vysledky"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy">
+	<li <?php if ($actual == "vysledky"){?>id="aktivni" <?php } else { ?> id="neaktivni" <?php } ?> class="odkazy" onclick="location.href = 'vysledky.php';">
 		   	<img src="img/vysledky.png" style="height:17px;width:17px" href="vysledky.php">
 	<a class="odkaz1" href="index.php?stranka=vysledky">Výsledky</a>
    </li>
 </ul>
+		</div>
 	<div class="clear"></div>
 	</header>
