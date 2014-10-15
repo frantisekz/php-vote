2ad0db17b3d3981d29dd20ee46531f6ec839af30
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-10-15 08:32:04 +0200

    Voting

--- admin/uvod.php
+++ admin/uvod.php
@@ -8,14 +8,3 @@ if (isset($_POST["username_register"]))
 	register($_POST["username_register"], $_POST["username_password"], $_POST["username_mail"], $_POST["username_level"]);
 }
 ?>
-
-<h2>Vytvořit uživatele</h2>
-
-<form method="POST">
-	<input class="kod" type="textfield" name="username_register" size="20" placeholder="Uživatelské jméno">
-	<input class="kod" type="textfield" name="username_password" size="20" placeholder="Heslo uživatele">
-	<input class="kod" type="textfield" name="username_mail" size="20" placeholder="E-mail uživatele">
-	<input class="kod" type="textfield" name="username_level" size="20" placeholder="Úroveň uživatele">
-	<div class="mezera"></div>
-    <input class="vstoupit" type="submit" value="Vytvořit účet" name="JPW">
-</form>
