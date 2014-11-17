7687b0483c035b62953b0e8b3d85cc32b405a19c
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-11-17 23:38:51 +0100

    User levels adjust

--- admin/uzivatele.php
+++ admin/uzivatele.php
@@ -9,9 +9,8 @@
 	<input class="kod" type="textfield" name="username_mail" size="20" placeholder="E-mail uživatele">
 	<br/><br/>
 	<select class="level" name="username_level" size="1"> 
-		<option value="1" selected>1 - user</option>
-<option value="2">2 - moderator</option>
-<option value="3">3 - admin</option>
+<option value="2">Učitel</option>
+<option value="3">Správce</option>
 </select>
 	<div class="mezera"></div>
     <input class="registrovat" type="submit" value="Vytvořit účet" name="JPW">
