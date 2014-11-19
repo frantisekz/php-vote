2a8125ffb73968addf901f57cae8e05d63846e65
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-11-19 11:49:58 +0100

    FIx Register test on php 5.4

--- tests/registerTest.php
+++ tests/registerTest.php
@@ -6,14 +6,17 @@ class registerTest extends PHPUnit_Framework_TestCase {
     protected function setUp()
     {
         include("functions.php");
+        if (phpversion() < 5.5)
+        {
+            require_once ('../passwordLib.php');
+        }
         $this->user = new user("test", 1);
     }
 
     public function testRegister()
     {
         $this->assertTrue(register("test", "test", "test", 3));
-        // CLeanup
-        unlink("users/test.txt");
+        unlink("users/test.txt"); // CLeanup
     }
 }
 ?>
