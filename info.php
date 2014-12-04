466c6a8c85392297382aaab43a2850b98f795c3a
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2014-12-04 23:27:20 +0100

    -_-

--- info.php
+++ info.php
@@ -4,12 +4,14 @@ $actual = "info";
 <div class="clear">
 <div class="body">
 <div class="mezera"></div>
-<h5>Autoři: František Zatloukal, Kouzelník, Michael Kužela, Vojtěch Górecki</h5>
+<h5>Autoři: František Zatloukal, Radek Vojkůvka, Michael Kužela, Vojtěch Górecki</h5>
 <div style="height: 50px;"></div>
 <p>
 Tyto stránky byly vypracovány v rámci maturitní práce z IVT v roce 2014/2015.
 </p>
 <div class="mezera"></div>
+
+
 <div class="vstoupit-div">
 
 	</div>	
