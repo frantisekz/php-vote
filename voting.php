4e7f0169cd2f45a0a0cba5106f44996dad7d46ad
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2015-01-22 10:27:38 +0100

    Adapt frontend to graph changes

--- voting.php
+++ voting.php
@@ -52,7 +52,10 @@ if (isset($more[3]))
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?page=timeout">';
 	die();
 }
-
+echo '    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal1">
+      Zobrazit graf
+    </button>';
 $header = $voting->view_voting($_SESSION["voting_code"]);
 echo "	<div class='mezera'></div>";
 echo "<h10>" . $header . " - " . $voting->question_header($_SESSION["voting_code"], $_SESSION["question"]) . "</h10>";
@@ -70,5 +73,50 @@ foreach ($voting->get_possibilities($_SESSION["voting_code"], $_SESSION["questio
 $i=$i+1;
  echo '</div>';
 }
-echo '</div>';
+echo '</div>
+<!-- Modal -->
+  <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Graf k otázce: ' . $voting->question_header($_GET["voting_result"], $qid) . '</h4>
+        </div>
+        <div class="modal-body">
+          <div id="result">';
+            $count = 0;
+            $p = 0;
+            $voters = $voting->voters($_SESSION["voting_code"]);
+           echo '<h1>Správných hlasů: ' . $right . '</h1>
+            <fieldset class="graph">
+              <ul id="legenda">';
+                $p = 0;
+                foreach ($voters as $voter)
+                {
+                  $palette[] = random_color();
+                  echo '<li style="color:' . $palette[$p] . ';"><span class="question">' . $voter . '</span>';
+                  $p = $p + 1;
+                }
+                echo  '
+              </ul>
+              <div class="chart">';
+                $p = 0;
+                foreach ($voters as $voter)
+                {
+                  $count = $voting->count_answered_right($_SESSION["voting_code"], $voter);
+                  echo '<div style="width: ' . ($count * 10) . 'px;background-color:' . $palette[$p] . '">' . $count . '</div>';
+                  $p = $p + 1;
+                }
+                echo '
+                </div>
+              </fieldset>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Zavřít</button>
+          </div>
+        </div>
+      </div>
+    </div>
+';
 ?>
