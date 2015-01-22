d957f2449d14bbd3625a14ce5feb56e25ffde2a5
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2015-01-22 22:36:54 +0100

    Fix in voting graph

--- voting.php
+++ voting.php
@@ -87,7 +87,7 @@ echo '</div>
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Graf k otázce: ' . $voting->question_header($_GET["voting_result"], $qid) . '</h4>
+          <h4 class="modal-title" id="myModalLabel">Průběžný graf správných odpovědí</h4>
         </div>
         <div class="modal-body">
           <div id="result">';
