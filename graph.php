5fda911b24fb0d6bd9924ea6226dba009637efc9
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2015-02-10 21:48:04 +0100

    Allow viewing results to visitors

--- graph.php
+++ graph.php
@@ -1,16 +1,24 @@
+<div id="result">
 <?php
-echo '	 
-<div id="result">';
-$count = 0;
-$p = 0;
-$voters = $voting->voters($_GET["voting_result"]);
-echo '<h1>Graf počtu správných hlasů</h1>
+if (!isset($_POST["voting_code"]))
+{
+	$code = $_GET["voting_result"];
+}
+else
+{
+	$code = $_POST["voting_code"];
+}
+$voters = $voting->voters($code);
+?>
+
+<h1>Graf počtu správných hlasů</h1> - <strong>Hlasování číslo <?php echo $code ?></strong>
 <fieldset class="graph">
-  <ul id="legenda">';
+  <ul id="legenda">
+  	<?php
 	$p = 0;
 	foreach ($voters as $voter)
 	{
-		$count = $voting->count_answered_right($_GET["voting_result"], $voter);
+		$count = $voting->count_answered_right($code, $voter);
 		$right = $right + $count;
 		$palette[] = random_color();
 		echo '<li style="color:' . $palette[$p] . ';"><span class="question">' . $voter . '</span>';
@@ -20,9 +28,10 @@ echo '<h1>Graf počtu správných hlasů</h1>
   </ul>
   <div class="chart">';
 	$p = 0;
+	$count = 0;
 	foreach ($voters as $voter)
 	{
-		$count = $voting->count_answered_right($_GET["voting_result"], $voter);
+		$count = $voting->count_answered_right($code, $voter);
 		if ($count != 0)
 		{
 			$percent = ($count * 100) / $right;
@@ -34,9 +43,7 @@ echo '<h1>Graf počtu správných hlasů</h1>
 		echo '<div style="width: ' . round(($percent * 4)) . 'px;background-color:' . $palette[$p] . '">' . $count . '</div>';
 		$p = $p + 1;
 	}
-	echo '
+	?>
 	</div>
   </fieldset>
 </div>
-';
-?>
---
+++ results.php
@@ -0,0 +1,6 @@
+<div class="clear"></div>
+<form method="POST" action="index.php?page=graph">
+	<input class="kod" type="text" name="voting_code" size="20" placeholder="Kód k hlasování" autocomplete="off">
+	<div class="mezera"></div>
+    <input class="vstoupit" type="submit" value="Zobrazit výsledky" name="JPW">
+</form>
