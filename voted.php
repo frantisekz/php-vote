70b8b947d4821f046dfba7b33bf95ac2353cf3f0
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2015-03-23 22:23:58 +0100

    Comment more voting, check if user can vote

---
+++ voted.php
@@ -0,0 +1,12 @@
+<div class="clear">
+<div class="body">
+<?php
+$actual = "timeout";
+?>
+<h8>Hlasovat lze jen jednou!</h8>
+	<div style="height: 30px;"></div>
+	</div>	
+	<div class="mezera"></div>
+        </div>
+	<div style="height: 30px;"></div>
+<img src="img/logo_GK.png" id="logo">
--- voting.php
+++ voting.php
@@ -8,10 +8,14 @@ if ((!isset($_POST["voting_code"])) AND (!isset($_SESSION["voting_code"])))
 	// Somebody tried to load file directly, die in pain!
 	die();
 }
+
+// Set SESSION to count questions
 if (!isset($_SESSION["question"]))
 {
 	$_SESSION["question"] = 1;
 }
+
+// Set SESSION variable to hold username
 if (!isset($_SESSION["voting_user"]))
 {
 	if ((isset($_COOKIE["computer_id"])) AND (is_numeric($_COOKIE["computer_id"])))
@@ -23,12 +27,17 @@ if (!isset($_SESSION["voting_user"]))
 		$_SESSION["voting_user"] = $_POST["voting_user"];
 	}
 }
+
+// Set SESSION variable to hold voting code
 if (!isset($_SESSION["voting_code"]))
 {
 	$_SESSION["voting_code"] = $_POST["voting_code"];
 }
+
+// Load voting info file, just in case something gonna need this
 $more = $voting->get_more($_SESSION["voting_code"]);
-// Check if somebody voted
+
+// Check if somebody actually voted
 if (isset($_GET["vote"]))
 {
 	$voting->write_vote($_SESSION["voting_user"], $_SESSION["voting_code"], $_SESSION["question"] , $_GET["vote"]);
@@ -39,22 +48,40 @@ if (isset($_GET["vote"]))
 	}
 	$_SESSION["question"] = $_SESSION["question"] + 1;
 }
+
+// Whoops, bad code
 if ((isset($_SESSION["voting_code"])) AND ($voting->voting_exists($_SESSION["voting_code"]) != 1))
 {
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?page=password">';
 	die();
 }
+
+// Whoops, too late
 if ($more[3] == 0)
 {
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?page=timeout">';
 	die();
 }
+
+// Check just for the first question and only once in one SESSION if user voted already
+if (($voting->answered($_SESSION["voting_code"], 1, $_SESSION["voting_user"]) != true))
+{
+	// So, user didn't voted here yet, let him go
+	$_SESSION["user_passed"] = 1;
+}
+
+elseif(!isset($_SESSION["user_passed"]))
+{
+	// User voted already here, kick him out
+	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?page=voted">';
+}
+
 $header = $voting->view_voting($_SESSION["voting_code"]);
 if (!isset($_SESSION["decrease"]))
 {
 	$_SESSION["decrease"] = 0;
 }
-// Check if question exists
+// Check if question exists so we don't need to renumber questions
 while (!$voting->question_exists($_SESSION["voting_code"], $_SESSION["question"]))
 {
 	$_SESSION["question"] = $_SESSION["question"] + 1;
