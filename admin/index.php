ff40762a35c55de6f2fb32d792f4650e5a37b945
František Zatloukal <Zatloukal.Frantisek@gmail.com>
2015-03-23 21:49:34 +0100

    Simple changes saved indicator

--- admin/index.php
+++ admin/index.php
@@ -48,24 +48,28 @@ if (isset($_POST['username_logout']))
 if (isset($_POST["voting_name"]))
 {
 	$voting->create_voting($_POST["voting_name"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 }
 
 if (isset($_POST["computer_id"]))
 {
 	set_cookie($_POST["computer_id"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?sub=settings">';
 }
 
 if (isset($_GET["unset_cookie"]))
 {
 	unset_cookie($_POST["computer_id"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?sub=settings">';
 }
 
 if (isset($_POST["question_name_edit"]))
 {
 	$voting->question_edit($_GET["voting_edit"], $_GET["edit_question"], $_POST["question_name_edit"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?voting_edit=' . $_GET["voting_edit"] . '">';
 }
 
@@ -95,43 +99,51 @@ if (isset($_POST["question_name"]))
 		$i = $i + 1;
 	}
 	$voting->add_question($_GET["voting_edit"], $_POST["question_name"], $possibilities, $_POST["possibility_right"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_POST["new_name"]))
 {
 	$user->edit_user($_GET["user_edit"], $_POST["new_name"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_POST["new_email"]))
 {
 	$user->re_email($_GET["user_edit"], $_POST["new_email"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_POST["new_password"]))
 {
 	$user->re_password($_GET["user_edit"], $_POST["new_password"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_GET["remove_question"]))
 {
 	$voting->remove_question($_GET["voting_edit"], $_GET["remove_question"]);
 	$voting->renumber_questions($_GET["voting_edit"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_GET["voting_remove"]))
 {
 	$voting->delete_voting($_GET["voting_remove"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 }
 
 if (isset($_POST["possibility_new"]))
 {
 	$voting->add_possibility($_GET["voting_edit"], $_GET["edit_question"], $_POST["possibility_new"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 if (isset($_GET["right_possibility"]))
 {
 	$voting->possibility_right($_GET["voting_edit"], $_GET["edit_question"], $_GET["right_possibility"]);
+	$_SESSION["change_ok"] = 1;
 }
 
 // Determine existing edit_possibility
@@ -144,6 +156,7 @@ if (isset($_GET["edit_question"])) // Dont go through if $_GET["edit_question"] 
 		if ((isset($_POST["edit_possibility_" . $i])) AND ($_POST["edit_possibility_" . $i] != ""))
 		{
 			$voting->possibility_edit($_GET["voting_edit"], $_GET["edit_question"], $i, $_POST["edit_possibility_" . $i]);
+			$_SESSION["change_ok"] = 1;
 		}
 		$i = $i + 1;
 	}
@@ -152,41 +165,43 @@ if (isset($_GET["edit_question"])) // Dont go through if $_GET["edit_question"] 
 
 if (isset($_GET["remove_possibility"]))
 {
-	$voting->remove_possibility($_GET["voting_edit"], $_GET["edit_question"], $_GET
-
-["remove_possibility"]);
-	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?voting_edit='. $_GET
-
-["voting_edit"] . '&edit_question=' . $_GET["edit_question"] .'">';
+	$voting->remove_possibility($_GET["voting_edit"], $_GET["edit_question"], $_GET["remove_possibility"]);
+	$_SESSION["change_ok"] = 2;
+	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?voting_edit='. $_GET["voting_edit"] . '&edit_question=' . $_GET["edit_question"] .'">';
 }
 
 if (isset($_POST["username_register"]))
 {
 	register($_POST["username_register"], $_POST["username_password"], $_POST["username_mail"], $_POST["username_level"], 1);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0">';
 }
 
 if (isset($_GET["voting_duplicate"]))
 {
 	$voting->duplicate_voting($_GET["voting_duplicate"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 }
 
 if (isset($_GET["voting_lock"]))
 {
 	$voting->voting_lock($_GET["voting_lock"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 }
 
 if (isset($_GET["voting_unlock"]))
 {
 	$voting->voting_unlock($_GET["voting_unlock"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';
 }
 
 if (isset($_GET["user_remove"]))
 {
 	$user->delete_user($_GET["user_remove"]);
+	$_SESSION["change_ok"] = 2;
 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php?sub=users">';
 }
 ?>
@@ -208,7 +223,19 @@ echo " id=\"active\" "?>><a href="index.php?sub=users">Uživatelé</a></h3>
 
 <div class="admin">
 <?php
-
+if (isset($_SESSION["change_ok"]))
+{
+	if ($_SESSION["change_ok"] == 2)
+	{
+		$_SESSION["change_ok"] = 1;
+	}
+	else
+	{
+		echo '<div class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Zavřít"><span aria-hidden="true">&times;</span></button>Změna byla úspěšně provedena.</div>';
+		unset($_SESSION["change_ok"]);		
+	}
+}
 switch ($_GET['sub']){
 	case "users":
 		include('users.php');
